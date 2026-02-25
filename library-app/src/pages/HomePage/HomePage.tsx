f769cda0db7bd24e8b7ca47de14f78bfcfc526e7
Ishan Hasan <84034201+IT-HASAN@users.noreply.github.com>
2026-02-25 12:42:12 +0000

    Setup Home page

--- library-app/src/pages/HomePage/HomePage.tsx
+++ library-app/src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,20 @@
 import type { JSX } from 'react';
+import { BookOfTheWeek, UpcomingEvents, LibraryCard, LibraryHours, ContactUs } from '../../features/landing';
+
+import './HomePage.css';
 
 export default function HomePage():JSX.Element {
-
   return (
-    <div className="page" style={{height:"100vh"}}>
-      Home page
+    <div className="home-page-container">
+      <div className="home-page-left">
+        <BookOfTheWeek />
+        <UpcomingEvents />
+        <LibraryCard />
+      </div>
+      <div className="home-page-right">
+        <LibraryHours />
+        <ContactUs />
+      </div>
     </div>
   )
 }
